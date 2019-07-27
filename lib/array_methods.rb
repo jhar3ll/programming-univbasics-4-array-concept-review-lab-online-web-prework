d563a578ed57e09194211d373a1e6380a16d1a8f
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 19:52:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,6 @@
 def find_element_index(array, value_to_find)
-  array = [2,3,4,5]
-  
-  p array.index(2)
-  p array.index(4)
-  
-  array_2 = [6,5,4,3]
-  p array.index(7)
-    
+  elemtent = array.index(value_to_find)
+  return elemtent
 end
+
+p array.index([2,3,4,5], 2)
