2a574281285324b04023146009f8e9fa40fb60fc
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 20:00:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,15 +9,15 @@ p find_element_index([6,5,4,3], 5)
 p find_element_index([6,5,4,3], 6)
 
 def find_max_value(array)
-  
-  
+  array_max = array.max
+  return array_max
+end  
 
-
-[2,3,4,5]
-[6,5,4,3]
-[1,2,3,4,5,4,3,2,1]
-[5,4,3,2,1,0,1,2,3,4,5]
-[500,4,1000,5,250]
+p find_max_value([2,3,4,5])
+p find_max_value([6,5,4,3])
+p find_max_value([1,2,3,4,5,4,3,2,1])
+p find_max_value([5,4,3,2,1,0,1,2,3,4,5])
+p find_max_value([500,4,1000,5,250])
   
   
   
