37b7aa83bada8e9b3b4faed4d9895a5ad9cbe22f
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 20:00:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,4 +20,15 @@ p find_max_value([5,4,3,2,1,0,1,2,3,4,5])
 p find_max_value([500,4,1000,5,250])
   
   
+  def find_mmin_value(array)
+  array_max = array.max
+  return array_max
+end  
+
+p find_max_value([2,3,4,5])
+p find_max_value([6,5,4,3])
+p find_max_value([1,2,3,4,5,4,3,2,1])
+p find_max_value([5,4,3,2,1,0,1,2,3,4,5])
+p find_max_value([500,4,1000,5,250])
+  
   
