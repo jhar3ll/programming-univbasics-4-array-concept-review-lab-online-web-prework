f555042de902fd6666ff9afeb9f2da2195b36745
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 19:42:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def find_element_index(array, value_to_find)
-  scale = [2,3,4,5]
-  p scale.index(2)
+  array = [2,3,4,5]
+  p array.index(2)
+
+end   
+  p array = [2,3,4,5]
+  p array.index(4)
   
-  scale = [2,3,4,5]
-  p scale.index(4)
-  
-  scale = [2,3,4,5]
-  p scale.index(7)
+  array  = [2,3,4,5]
+  p array.index(7)
     
 end
